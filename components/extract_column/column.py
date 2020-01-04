b8f6207894c8f71803c1ccf7924da0351aa0c80c
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2020-01-04 17:47:51 -0600

    temporary implementation of extracting column

--- components/extract_column/column.py
+++ components/extract_column/column.py
@@ -12,6 +12,32 @@ class Column:
         self.ending_cell: CellCompact = None
         self.type: ContentType = None
 
+    def convert_cell_type(self, cell: CellCompact, max_type: ContentType) -> bool:
+        if cell.content_type == max_type:
+            return True
+        if max_type == ContentType.STRING:
+            if cell.content_type == ContentType.NUMERIC:
+                try:
+                    cell.content = str(cell.content)
+                    cell.content_type = ContentType.STRING
+                except:
+                    return False
+            elif cell.content_type == ContentType.NULL:
+                return True
+        elif max_type == ContentType.NUMERIC:
+            if cell.content_type == ContentType.STRING:
+                try:
+                    cell.content = float(cell.content)
+                    cell.content_type = ContentType.STRING
+                except:
+                    return False
+            elif cell.content_type == ContentType.NULL:
+                return True
+        elif max_type == ContentType.NULL:
+            cell.content_type = ContentType.NULL
+            return True
+        return False
+
     def set_type(self):
         if len(self.content_cells) == 0:
             self.type = None # blank column does not have any content
@@ -28,4 +54,14 @@ class Column:
             if value > max_count:
                 max_count = value
                 max_type = key
-
+        failed_cells = []
+        for cell in self.content_cells:
+            if not self.convert_cell_type(cell, max_type):
+                failed_cells.append(cell)
+        if len(failed_cells) / len(self.content_cells) >= 0.05:
+            for cell in self.content_cells:
+                if not self.convert_cell_type(cell, ContentType.STRING):
+                    raise Exception("Fail to convert all cells to string")
+        else:
+            for cell in failed_cells:
+                self.convert_cell_type(cell, ContentType.NULL)
