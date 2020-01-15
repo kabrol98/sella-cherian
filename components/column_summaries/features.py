c88b44d447f0dbddc36fd93d6d9620141ae7d9ba
taozi <yitaom2@illinois.edu>
2020-01-15 18:52:20 +0800

    column_summaries

--- components/column_summaries/features.py
+++ components/column_summaries/features.py
@@ -1,31 +1,63 @@
-from ..extract_column.column import Column
-from ..extract_column.metadata import Metadata
+from components.extract_column.column import Column
+from components.cell_labeling.cell_compact import CellCompact, ContentType, CellTagType
+from components.parse_files.metadata import ColumnMetaData, FileMetaData
+
+from collections import Counter
 import numpy as np
 
 class Features:
-    def __init__(self, column: Column, metadata: Metadata):
-        #data_type: using 0 for data 1 for string
-        self.revised_data, self.data_type = self.determine_type(column)
+    def __init__(self, column: Column, file_metadata: FileMetaData):
+        self.data_type = column.type
+        # -- only for column.type == ContentType.STRING
         self.value_range = 0
         self.max = 0
         self.min = 0
         self.mean = 0
         self.std = 0
+        #
         self.common_values0 = 0
         self.common_frequencies0 = 0
         self.common_values1 = 0
         self.common_frequencies1 = 0
         self.unique_num = 0
         self.null_num = 0
-        self.mean = 0
         #metadata:
-        self.metadata: metadata
+        self.column_metadata = column.metadata
+        self.file_metadata = file_metadata
         
-        #revise most of the features based on self.revised_data
-        self.revise_features()
+        self.revise_features(column)
 
-    def determine_type(self, column):
-        content_list = []
-        return content_list, 0
-
-    def revise_features(self):
+    def revise_features(self, column):
+        if column.type == None:
+            return
+        if column.type == ContentType.NULL:
+            null_num = len(column)
+            return
+        count_null = 0
+        none_null_cell_values = []
+        for compact_cell in column.content_cells:
+            if compact_cell.content_type == ContentType.NULL:
+                count_null += 1
+            else:
+                none_null_cell_values.append(compact_cell.content)
+        self.null_num = count_null
+        if len(none_null_cell_values) == 0:
+            return
+        none_null_cell_values = np.array(none_null_cell_values)
+        if column.type == ContentType.NUMERIC:
+            self.max = np.max(none_null_cell_values)
+            self.min = np.min(none_null_cell_values)
+            self.value_range = self.max - self.min
+            self.mean = np.mean(none_null_cell_values)
+            self.std = np.std(none_null_cell_values)
+        self.unique_num = len(np.unique(np.array(none_null_cell_values)))
+        try:
+            frequent_two = Counter(none_null_cell_values).most_common(2)
+            self.common_values0 = frequent_two[0][0]
+            self.common_frequencies0 = frequent_two[0][1]
+            self.common_values1 = frequent_two[1][0]
+            self.common_frequencies1 = frequent_two[1][1]
+        except:
+            frequent_one = Counter(none_null_cell_values).most_common(1)
+            self.common_values0 = frequent_two[0][0]
+            self.common_frequencies0 = frequent_two[0][1]
