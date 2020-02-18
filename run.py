bf5fbd2a417fa4e0f5752de5f97a1ee6a9e4aa0f
taozi <yitaom2@illinois.edu>
2020-02-17 19:11:54 -0600

    yitao

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import keras.backend as K
 
 from components.parse_files.parser import Parser
 from components.utils.test import SimpleTest
+from components.column_summaries.features import Features
 
 with SimpleTest():
     files = ["data_corpus/00sumdat_copy.xlsx", "data_corpus/1-1-10Na-Kinetic Curves.xlsx", "data_corpus/Aexperiment.xlsx"]
@@ -15,5 +16,9 @@ with SimpleTest():
 
     parser = Parser(files[0], model)
     for res in parser.parse():
-        print(res)
-
+        # print(res)
+        feature = Features(res, None)
+        # print(feature.data_type, feature.value_range, feature.max, feature.min, feature.mean,
+        #         feature.std, feature.mask, feature.common_values0, feature.common_frequencies0,
+        #         feature.common_values1, feature.common_frequencies1, feature.unique_num, feature.null_num,
+        #         feature.column_metadata, feature.file_metadata, "\n")
