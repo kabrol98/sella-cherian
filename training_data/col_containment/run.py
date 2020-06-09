0a6edd921dce989a43c69902990e276c86aedce7
Your Name <tailinz2@illinois.edu>
2020-06-09 12:15:03 -0500

    more data

--- training_data/col_containment/run.py
+++ training_data/col_containment/run.py
@@ -29,17 +29,7 @@ for file in files:
         newsheet = newfile.add_sheet(sheet.name)
 
         rows, cols = sheet.nrows, sheet.ncols
-        new_rows = 0
         new_cols = 0
-        new_rows_list = []
-
-        for row in range(rows):
-            row_selection = random.random()
-            if new_rows != 0 or row != rows - 1:
-                if row_selection > border:
-                    continue
-            new_rows_list += [row]
-            new_rows += 1
 
         for col in range(cols):
             col_selection = random.random()
@@ -48,11 +38,8 @@ for file in files:
                     continue
             record_2 = file + ", " + sheet.name + ", " + str(col) + ", " + newfile_name + ", " + sheet.name + ", " + str(new_cols) + ", 1\n"
             record_file2.write(record_2)
-
-            new_rows = 0
-            for row in new_rows_list:
-                newsheet.write(new_rows, new_cols, sheet.cell(row, col).value)
-                new_rows += 1
+            for row in range(rows):
+                newsheet.write(row, new_cols, sheet.cell(row, col).value)
             new_cols += 1
 
     newfile.save("data/" + newfile_name)
